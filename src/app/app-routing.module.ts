0b0d4c7534bea62ccac75e10c20e653f9c24641e
ann-wangui <wanguia822@gmail.com>
2022-04-27 12:56:04 +0300

    generate user repositories component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  // { path:"User",component:}, 
+ 	// { path:"Repositories",component:}, 
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+  CommonModule,
+  RouterModule.forRoot(routes)
+],
+  
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
+
