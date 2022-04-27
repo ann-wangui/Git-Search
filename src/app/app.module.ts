314ce6d41c830970f5b6d88bd9b16b9892e060de
ann-wangui <wanguia822@gmail.com>
2022-04-27 12:52:55 +0300

    nested user repositories component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RepositoriesComponent } from './repositories/repositories.component';
 import { UserRepositoriesComponent } from './user-repositories/user-repositories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RepositoriesComponent,
     UserRepositoriesComponent
   ],
   imports: [
