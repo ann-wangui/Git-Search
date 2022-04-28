80ba01b7e3f3a6242e0341e3fb405e9e7c101330
ann-wangui <wanguia822@gmail.com>
2022-04-28 11:32:06 +0300

    import httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { UserRepositoriesService } from './services/user-repositories.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,13 +11,15 @@ import { UserRepositoriesComponent } from './user-repositories/user-repositories
 @NgModule({
   declarations: [
     AppComponent,
-    UserRepositoriesComponent
+    UserRepositoriesComponent,
+    FormsModule,
+    HttpClientModule
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UserRepositoriesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
